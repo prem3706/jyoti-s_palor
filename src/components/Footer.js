82b6f7fcf2f9bfe92aca608e887f4f5666622c7c
prem3706 <parateprem3706@gmail.com>
2025-12-14 15:22:21 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer(){
   return (
     <footer className='bg-white border-t mt-12'>
       <div className='max-w-6xl mx-auto px-4 py-6 text-center text-gray-600'>
-        © {new Date().getFullYear()} Jyoti's Beauty Parlour • Created by Prem
+        © {new Date().getFullYear()} Jyoti Beauty Parlour • Created by Prem
       </div>
     </footer>
   );
